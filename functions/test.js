5306fda30fd36acee0ce8594978eff825c024b7b
James <beattie497@gmail.com>
2020-04-23 19:31:13 +0100

    j

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,25 @@
-exports.handler = (event, context, callback) => {
-    callback(null, {
-      statusCode: 200,
-      body: 'No worries, all is working fine!'
+const fetch = require('node-fetch')
+
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+
+exports.handler = async (event, context) => {
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+    // handle response
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
     })
   }
+}
