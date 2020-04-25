adad4f8db54f29ae9c40b955cc081cf0d5f2535c
James <beattie497@gmail.com>
2020-04-25 21:09:27 +0100

    ETAG TEST

--- functions/test.js
+++ functions/test.js
@@ -26,7 +26,7 @@ exports.handler = function(event, context, callback){
   //Personming api call
 
   const getVideos = () => {
-    axios.get(URL)
+    axios.get(URL , {headers: { "If-None-Match":'nxOHAKTVB7baOKsQgTtJIyGxcs8/pYDE6vjmwEGNxgI3RViaQe6RFLQ'}})
       .then(res => send(res.data));
 
   } 
