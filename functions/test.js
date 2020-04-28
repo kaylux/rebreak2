3187d8f4ee364f939c36cee6417c1f1c976cfe74
James <beattie497@gmail.com>
2020-04-28 21:48:13 +0100

    fail

--- functions/test.js
+++ functions/test.js
@@ -9,8 +9,6 @@ exports.handler = function(event, context, callback){
   const MAX_RESULTS = '10';
 
  
-
-  const URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=AIzaSyC5UTeickpgiE_xSIXJqDZXMZ5rzq9Ty00&maxResults=25&channelId=UCwIxn6d5t7gZvebnGUoWJ3A&order=date';
   const URL = `'${API_URL}?part=snippet&type=video&key=${API_KEY}&maxResults=${MAX_RESULTS}&channelId=${CHANNEL_ID}&order=date'`;
 
   //SEND user response
