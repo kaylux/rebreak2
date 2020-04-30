8a1b17dd8eeb92823aec2036731d68506f6a3298
James <beattie497@gmail.com>
2020-04-30 10:32:24 +0100

    test

--- functions/mostpopular.js
+++ functions/mostpopular.js
@@ -4,14 +4,16 @@ const axios = require('axios');
 
 exports.handler = function(event, context, callback){
   const {API_URL, CHANNEL_ID, API_KEY} = process.env;
+  const videoIds = event.queryStringParameters.videoIds;
 
   const MAX_RESULTS = '10';
   const UPLOADS_PLAYLIST = 'UUwIxn6d5t7gZvebnGUoWJ3A';
 
   //const URL = `${API_URL}?part=snippet&type=video&key=${API_KEY}&maxResults=${MAX_RESULTS}&channelId=${CHANNEL_ID}&order=date`;
-  const URL = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=UUwIxn6d5t7gZvebnGUoWJ3A&fields=items(contentDetails(videoId%2CvideoPublishedAt)%2Csnippet%2Ftitle%2Cstatus)&maxResults=25&key=AIzaSyC5UTeickpgiE_xSIXJqDZXMZ5rzq9Ty00';
+  const URL = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=DqL8Lcx3NPo%2CPfQazWP8PpY%2CfQoDsd1fr3o%2C2LxdmkPNsfE%2Cbkjh--XsoQo%2CJhoUlLaMDMY%2C6cX0llvY7Sk%2CuSqKtMHh3yQ%2CILHdTX-hEY8%2C1g5ykWTq3ZM%2CKoBZq29wClw%2CPBcVFjMHOSo%2CwR1KMvUG0M0%2C4gYhkDLySdc%2Csp_yQtIQ-6A&key=AIzaSyC5UTeickpgiE_xSIXJqDZXMZ5rzq9Ty00';
 
- 
+  
+
   const send = body => {
   callback(null, {
       statusCode: 200,
@@ -22,16 +24,10 @@ exports.handler = function(event, context, callback){
 
   }
 
-  //Personming api calls
+  //Personming api call
+
   const getVideos = () => {
     axios.get(URL , {headers: { "If-None-Match":'nxOHAKTVB7baOKsQgTtJIyGxcs8/pYDE6vjmwEGNxgI3RViaQe6RFLQ'}})
-      .then(res => getViews(res.data));
-
-  } 
-
-
-  const getViews = ids => {
-    axios.get(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${ids}&key=AIzaSyC5UTeickpgiE_xSIXJqDZXMZ5rzq9Ty00` , {headers: { "If-None-Match":'nxOHAKTVB7baOKsQgTtJIyGxcs8/pYDE6vjmwEGNxgI3RViaQe6RFLQ'}})
       .then(res => send(res.data));
 
   } 
