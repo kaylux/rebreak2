02a0e6ffe35db6b53706d0c7748b0f0e529b4396
James <beattie497@gmail.com>
2020-04-30 02:22:23 +0100

    dd

--- functions/mostpopular.js
+++ functions/mostpopular.js
@@ -30,8 +30,8 @@ exports.handler = function(event, context, callback){
   } 
 
 
-  const getViews = (res) => {
-    axios.get(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${res}&key=AIzaSyC5UTeickpgiE_xSIXJqDZXMZ5rzq9Ty00` , {headers: { "If-None-Match":'nxOHAKTVB7baOKsQgTtJIyGxcs8/pYDE6vjmwEGNxgI3RViaQe6RFLQ'}})
+  const getViews = ids => {
+    axios.get(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${ids}&key=AIzaSyC5UTeickpgiE_xSIXJqDZXMZ5rzq9Ty00` , {headers: { "If-None-Match":'nxOHAKTVB7baOKsQgTtJIyGxcs8/pYDE6vjmwEGNxgI3RViaQe6RFLQ'}})
       .then(res => send(res.data));
 
   } 
